766db1e6e1b6b0791a670a43369bf62286ba9512
DanielSchmidt <93592465+DanielSchmidt1@users.noreply.github.com>
2024-06-10 14:16:02 +0200

    added navbar
    navbar.js added and imported (+ added) in app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from './components/login';
 import Logout from './components/logout';
 import Single from './components/single';
 import Search from './components/Search';
+import Navbar from './components/Navbar';
 //Admin imports
 import Admin from './Admin';
 import Create from './components/admin/create';
@@ -29,6 +30,7 @@ function App() {
           <Route path='/logout' element={<Logout />} />
           <Route path='/blog/:slug' element={<Single />} />
           <Route path='/search' element={<Search />} />
+          <Route path='/navbar' element={<Navbar />} />
         </Routes>
       <Footer />
     </Router>
---
+++ src/components/Navbar.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { NavLink } from 'react-router-dom';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
+
+
+function Navbar() {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Drawer
+                variant="permanent"
+                sx={{
+                    width: 240,
+                    flexShrink: 0,
+                    [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
+                }}
+            >
+                <Box sx={{ overflow: 'auto' }}>
+                    <List>
+                        <ListItemButton component={NavLink} to="/home">
+                            <ListItemText primary="E-Commerce Dashboard" />
+                        </ListItemButton>
+                    </List>
+                    <Divider />
+                    <List>
+                        <ListItemButton component={NavLink} to="/dashboard">
+                            <ListItemText secondary="Dashboard" />
+                        </ListItemButton>
+                        <ListItemButton component={NavLink} to="/map">
+                            <ListItemText secondary="Map" />
+                        </ListItemButton>
+                        <ListItemButton component={NavLink} to="/salesgraph">
+                            <ListItemText secondary="Salesgraph" />
+                        </ListItemButton>
+                        <ListItemButton component={NavLink} to="/products">
+                            <ListItemText secondary="Products" />
+                        </ListItemButton>
+                        <ListItemButton component={NavLink} to="/customer">
+                            <ListItemText secondary="Customer" />
+                        </ListItemButton>
+                        <Divider />
+                    </List>
+                    <List>
+                        <ListItemButton component={NavLink} to="/register">
+                            <ListItemText secondary="Register" />
+                        </ListItemButton>
+                        <ListItemButton component={NavLink} to="/login">
+                            <ListItemText secondary="Login" />
+                        </ListItemButton>
+                        <ListItemButton component={NavLink} to="/logout">
+                            <ListItemText secondary="Logout" />
+                        </ListItemButton>
+                    </List>
+                </Box>
+            </Drawer>
+        </React.Fragment>
+    );
+}
+export default Navbar;
